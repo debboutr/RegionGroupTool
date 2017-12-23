64089166fb54eabd5db08c1fc837e4b3cb5d7ead
rickD <debboutr@gmail.com>
2017-12-23 11:49:02 -0800

    remove list, add copy of arr for replace

--- region_group.py
+++ region_group.py
@@ -17,17 +17,17 @@ import georasters as gr
 from scipy.ndimage import label, generate_binary_structure
 
 def replace(arr, inc_val, no_data):
+    '''
+    replaces arr values in-place with inc_val
+    '''
+    fin = arr.copy()
     uni_arr = np.delete(np.unique(arr), no_data)
-    hold = []
     for num in uni_arr: 
-        a = arr.copy()
+        a = fin.copy()
         iso = a != num
         a[iso] = no_data 
         np.place(a, a==num, (num + inc_val))
-        hold.append(a)
-    for group in hold:
-        np.copyto(arr,group, where=group!=no_data)
-    return arr
+        np.copyto(arr,a, where=a!=no_data)
 
 def region_group(geo_raster, zone_connectivity=True):
     NDV = geo_raster.nodata_value
@@ -54,15 +54,15 @@ def region_group(geo_raster, zone_connectivity=True):
 
 if __name__ == '__main__':
     
-    orig = gr.from_file('carol.tif')
+    orig = gr.from_file('og.tif')
     out = region_group(orig)
-    out.to_tiff('north')
-
+    out.to_tiff('south')
+######################################################################
 
 #
 #NDV, xsize, ysize, GeoT, Projection, DataType = gr.get_geo_info('og.tif')
 #np.delete(np.unique(final), NDV)
-#
+
 #x = np.array([[0,0,0,2,2],
 #              [0,0,0,0,0],
 #              [0,2,2,0,0]])
@@ -70,3 +70,20 @@ if __name__ == '__main__':
 #x = np.array([[2,1,1,2,2],
 #              [0,1,1,0,3],
 #              [1,2,2,3,1]])
+    
+#    NDV = 0
+#    final = np.zeros(x.shape)
+#    values = np.delete(np.unique(x), NDV)
+#    if zone_connectivity:
+#        s = generate_binary_structure(2,2)
+#    incrementor = 0 # TODO: how should new values be generated???
+#    for val in values:
+#        single = x.copy()
+#        isolate = single != val
+#        single[isolate] = NDV
+#        grouped, num_feats = label(single, 
+#                                   structure=s if zone_connectivity else None)    
+#        if incrementor > 0:
+#            replace(grouped, incrementor, NDV)
+#        incrementor += num_feats
+#        np.copyto(final,grouped, where=grouped!=NDV)
