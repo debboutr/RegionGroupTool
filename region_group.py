06242d76cd19fd10a6197eeda1373f420001ceb5
rickD <debboutr@gmail.com>
2017-12-29 05:06:27 -0800

    switch to rasterio for more writeout options, possibly window???

--- region_group.py
+++ region_group.py
@@ -11,9 +11,8 @@ TODO: add check for dtype INT
     
 @author: rick
 """
-
 import numpy as np
-import georasters as gr
+import rasterio as rs
 from scipy.ndimage import label, generate_binary_structure
 
 def replace(arr, inc_val, no_data):
@@ -30,14 +29,15 @@ def replace(arr, inc_val, no_data):
         np.copyto(arr,a, where=a!=no_data)
 
 def region_group(geo_raster, zone_connectivity=True):
-    NDV = geo_raster.nodata_value
-    final = np.zeros(geo_raster.raster.data.shape)
-    values = np.delete(np.unique(geo_raster.raster.data), NDV)
+    NDV = data.nodata
+    final = np.zeros(data.shape)
+    array = data.read(1)
+    values = np.delete(np.unique(array), NDV)
     if zone_connectivity:
         s = generate_binary_structure(2,2)
     incrementor = 0 # TODO: how should new values be generated???
     for val in values:
-        single = geo_raster.raster.data.copy()
+        single = array.copy()
         isolate = single != val
         single[isolate] = NDV
         grouped, num_feats = label(single, 
@@ -47,16 +47,66 @@ def region_group(geo_raster, zone_connectivity=True):
         incrementor += num_feats
         np.copyto(final,grouped, where=grouped!=NDV)
     
-    return gr.GeoRaster(final,orig.geot, 
-                        nodata_value=geo_raster.nodata_value, 
-                        projection=geo_raster.projection, 
-                        datatype=geo_raster.datatype)
+    return final
 
 if __name__ == '__main__':
     
-    orig = gr.from_file('og.tif')
-    out = region_group(orig)
-    out.to_tiff('south')
+20295 * 29689
+        
+  25975  * 11375    
+         
+        
+        profile['dtype']
+type(rs.uint32)
+
+
+
+#########################################################################    
+#import numpy as np
+#import georasters as gr
+#from scipy.ndimage import label, generate_binary_structure
+#
+#def replace(arr, inc_val, no_data):
+#    '''
+#    replaces arr values in-place with inc_val
+#    '''
+#    fin = arr.copy()
+#    uni_arr = np.delete(np.unique(arr), no_data)
+#    for num in uni_arr: 
+#        a = fin.copy()
+#        iso = a != num
+#        a[iso] = no_data 
+#        np.place(a, a==num, (num + inc_val))
+#        np.copyto(arr,a, where=a!=no_data)
+#
+#def region_group(geo_raster, zone_connectivity=True):
+#    NDV = geo_raster.nodata_value
+#    final = np.zeros(geo_raster.raster.data.shape)
+#    values = np.delete(np.unique(geo_raster.raster.data), NDV)
+#    if zone_connectivity:
+#        s = generate_binary_structure(2,2)
+#    incrementor = 0 # TODO: how should new values be generated???
+#    for val in values:
+#        single = geo_raster.raster.data.copy()
+#        isolate = single != val
+#        single[isolate] = NDV
+#        grouped, num_feats = label(single, 
+#                                   structure=s if zone_connectivity else None)    
+#        if incrementor > 0:
+#            replace(grouped, incrementor, NDV)
+#        incrementor += num_feats
+#        np.copyto(final,grouped, where=grouped!=NDV)
+#    
+#    return gr.GeoRaster(final,orig.geot, 
+#                        nodata_value=geo_raster.nodata_value, 
+#                        projection=geo_raster.projection, 
+#                        datatype=geo_raster.datatype)
+#
+#if __name__ == '__main__':
+#    
+#    orig = gr.from_file('carol.tif')
+#    out = region_group(orig)
+#    out.to_tiff('new_carol')
 ######################################################################
 
 #
